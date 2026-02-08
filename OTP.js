08f6c444dd9e21fb59c175222e1da6b8d178f8ca
frkofficial <farukakram2000@gmail.com>
2026-02-08 12:04:43 +0600

    second commit

---
+++ OTP.js
@@ -0,0 +1,16 @@
+function validOtp(otp) {
+          
+    if(typeof otp !== 'string'){
+        return 'Invalid '
+    }
+
+    if(otp.length == 8 && otp.startsWith('ph-')){
+          return true
+    }
+    else{
+        return false 
+    }
+}
+
+const result = validOtp(["ph-10985"]);
+console.log(result);
